6792b2e5a635426116a032ec1cb3782a5d3998f7
J3sus777 <44508145+J3sus777@users.noreply.github.com>
2022-12-08 10:51:37 +0200

    Update main.cpp

--- CalculatorTest/main.cpp
+++ CalculatorTest/main.cpp
@@ -1,52 +1,56 @@
 #include "Interface.h"
 #include <gtest/gtest.h>
 
-TEST(CheckUP, OK)
+TEST(Interface, OK)
 {
     std::string example = "5+8*2-5/2";
     float result = 0;
-    CheckUP* ch = new CheckUP();
-    ch->checkUP(example, result);
+    Interface* interface = new Interface();
+    interface->checkUP(example, result);
     EXPECT_EQ(result, 18.5f);
 
     example = "2+2*2";
-    ch->checkUP(example, result);
+    interface->checkUP(example, result);
     EXPECT_EQ(result, 6);
 
     example = "2+(2 *3)";
-    ch->checkUP(example, result);
+    interface->checkUP(example, result);
     EXPECT_EQ(result, 8);
-    delete ch;
+    delete interface;
 }
 
-TEST(CheckUP, EceptionDuplicate)
+TEST(Interface, EceptionDuplicate)
 {
     std::string example = "5++8*2-5/2";
     float result = 0;
-    CheckUP* ch = new CheckUP();
-    ch->checkUP(example, result);
+    Interface* interface = new Interface();
+    interface->checkUP(example, result);
     EXPECT_EQ(example, "Error");
-    delete ch;
+    delete interface;
 }
 
-TEST(CheckUP, EceptionEmpty)
+TEST(Interface, EceptionEmpty)
 {
     std::string example = "";
     float result = 0;
-    CheckUP* ch = new CheckUP();
-    ch->checkUP(example, result);
+    Interface* interface = new Interface();
+    interface->checkUP(example, result);
     EXPECT_EQ(example, "Error");
-    delete ch;
+    delete interface;
 }
-TEST(CheckUP, ExceptionSymbol)
+TEST(Interface, ExceptionSymbol)
 {
     std::string example = "+8*2-5/2";
     float result = 0;
-    CheckUP* ch = new CheckUP();
+    Interface* interface = new Interface();
 
-    ch->checkUP(example, result);
+    interface->checkUP(example, result);
     EXPECT_EQ(example, "Error");
-    delete ch;
+    
+    std::string example = "ewf+8+3*(&^&*";
+    interface->checkUP(example, result);
+    EXPECT_EQ(example, "Error");
+    delete interface;
 }
 
 int main(int argc, char* argv[])
